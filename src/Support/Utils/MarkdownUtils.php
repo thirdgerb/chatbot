6b17f9625e80d9c078efeddfadd6a8a359e5c7ec
thirdgerb <thirdgerb@gmail.com>
2020-07-30 16:40:54 +0800

    增加了JsonMsg 类型的 verbal, 增加了 markdown 的方法

---
+++ src/Support/Utils/MarkdownUtils.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * This file is part of CommuneChatbot.
+ *
+ * @link     https://github.com/thirdgerb/chatbot
+ * @document https://github.com/thirdgerb/chatbot/blob/master/README.md
+ * @contact  <thirdgerb@gmail.com>
+ * @license  https://github.com/thirdgerb/chatbot/blob/master/LICENSE
+ */
+
+namespace Commune\Support\Utils;
+
+
+/**
+ * @author thirdgerb <thirdgerb@gmail.com>
+ */
+class MarkdownUtils
+{
+
+    public static function quote(string $text) : string
+    {
+        $lines = explode("\n", $text);
+        $output = implode("\n    ", $lines);
+        return "    $output";
+    }
+
+}
