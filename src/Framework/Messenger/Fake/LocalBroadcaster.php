76e71cf092c4dea5a1e2c4f7c607d8ad76c4ebee
thirdgerb <thirdgerb@gmail.com>
2020-08-04 18:39:54 +0800

    fix local broadcaster - forget to extends AbsBroadcaster

--- src/Framework/Messenger/Fake/LocalBroadcaster.php
+++ src/Framework/Messenger/Fake/LocalBroadcaster.php
@@ -15,13 +15,15 @@ use Commune\Blueprint\Kernel\Protocals\GhostRequest;
 use Commune\Blueprint\Kernel\Protocals\GhostResponse;
 use Commune\Blueprint\Shell;
 use Commune\Contracts\Messenger\Broadcaster;
+use Commune\Framework\Messenger\Broadcaster\AbsBroadcaster;
 use Commune\Kernel\Protocals\IShellOutputRequest;
+use Psr\Log\LoggerInterface;
 
 
 /**
  * @author thirdgerb <thirdgerb@gmail.com>
  */
-class LocalBroadcaster implements Broadcaster
+class LocalBroadcaster extends AbsBroadcaster
 {
     /**
      * @var Shell
@@ -34,40 +36,30 @@ class LocalBroadcaster implements Broadcaster
      * LocalBroadcaster constructor.
      * @param Shell $shell
      */
-    public function __construct(Shell $shell)
+    public function __construct(Shell $shell, LoggerInterface $logger)
     {
         $this->shell = $shell;
+        parent::__construct($logger, []);
     }
 
-
-    public function publish(
-        GhostRequest $request,
-        GhostResponse $response,
-        array $routes
-    ): void
-    {
-        $shellRequest = IShellOutputRequest::asyncInstance(
-            $request->getSessionId(),
-            $request->getTraceId(),
-            $request->getBatchId(),
-            $request->getInput()->getCreatorId(),
-            $request->getInput()->getCreatorName()
-        );
-
-        array_push($this->chan, $shellRequest);
-    }
-
-    public function subscribe(
+    public function doSubscribe(
         callable $callback,
         string $shellId,
         string $shellSessionId = null
     ): void
     {
         $chan = "$shellId/$shellSessionId";
-        while($request = array_shift($this->chan)) {
-            $callback($chan, $request);
+        while($message = array_shift($this->chan)) {
+            $callback($chan, $message);
         }
     }
 
-
+    public function doPublish(
+        string $shellId,
+        string $shellSessionId,
+        string $publish
+    ): void
+    {
+        array_push($this->chan, $publish);
+    }
 }
