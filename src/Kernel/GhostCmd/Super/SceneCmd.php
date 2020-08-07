72b3fddf5eeda8931c403e3c6f175dded072a7ac
thirdgerb <thirdgerb@gmail.com>
2020-08-07 18:25:46 +0800

    根据改动调整了 场景命令

--- src/Kernel/GhostCmd/Super/SceneCmd.php
+++ src/Kernel/GhostCmd/Super/SceneCmd.php
@@ -13,16 +13,19 @@ namespace Commune\Kernel\GhostCmd\Super;
 
 use Commune\Blueprint\Framework\Command\CommandMsg;
 use Commune\Blueprint\Framework\Pipes\RequestCmdPipe;
+use Commune\Blueprint\Ghost\Ucl;
 use Commune\Kernel\GhostCmd\AGhostCmd;
+use Commune\Support\Arr\ArrayAndJsonAble;
 
 
 /**
  * @author thirdgerb <thirdgerb@gmail.com>
  */
-class EnvCmd extends AGhostCmd
+class SceneCmd extends AGhostCmd
 {
-    const SIGNATURE = 'env
-       {--route : 当前会话的路由状态}
+    const SIGNATURE = 'scene
+       {--routes : 当前会话的路由状态}
+       {--awaits : 监听中的用户意图}
     ';
 
     const DESCRIPTION = '用于查看当前会话的环境变量';
@@ -47,16 +50,29 @@ class EnvCmd extends AGhostCmd
         }
 
         if ($matched === 0) {
-            $this->notice("没有任何有效的参数. 请输入 env -h 查看有效参数");
+            $this->notice("没有任何有效的参数. 请输入 -h 查看有效参数");
         }
     }
 
-    protected function __route() : string
+    protected function __routes() : string
     {
         $routes = $this->cloner->storage->shellSessionRoutes;
-        return "当前会话路由状态: " . json_encode($routes);
+        return "当前会话路由状态: " . json_encode(
+            $routes,
+            ArrayAndJsonAble::PRETTY_JSON
+        );
     }
 
+    protected function __awaits() : string
+    {
+        $awaits = $this->cloner->runtime->getCurrentAwaitRoutes();
+        $awaits = array_map([Ucl::class, 'encodeUcl'], $awaits);
+
+        return "当前监听意图: " . json_encode(
+            $awaits,
+            ArrayAndJsonAble::PRETTY_JSON
+        );
+    }
 
 
 }
